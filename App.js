269e097afc5923bed7802833b5ea39ff6f8f1881
Gabriel Gomes <58064090+g0m3s@users.noreply.github.com>
2020-05-31 22:11:02 -0300

    challenge weekend opening files

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   SafeAreaView,
@@ -18,28 +10,17 @@ import {
   Button,
 } from 'react-native';
 
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
 import Notification from './services/notification';
-// import notification from './services/notification';
 
-var titulos = ['ok 1','ok 2','ok 1','ok 2','ok 1','ok 2','ok 1','ok 2','ok 1','ok 2'];//titulos aleatorios
-var textos = ['ok 1','ok 2','ok 1','ok 2','ok 1','ok 2','ok 1','ok 2','ok 1','ok 2'];//mensagens que aparecerao
+var titulos = ['hi how are you?','How is your day going?','Have you taken a sun today?'];//titulos aleatorios
+var textos = ['how about taking the day to see that movie you love? it`s okay to take a day off and not be extremely productive','how about creating a routine to get organized in the home office? you can start your day by stretching','if you feel you need help, several psychologists are attending online. How about talking about what`s going on with someone prepared?'];//mensagens que aparecerao
 
 
 
 const App: () => React$Node = () => {
 
-    const getRandom = () => {
-
+    const getRandom = (max) => {
         return Math.floor(Math.random() * max + 1)
-
     }
 
   const createNotification = () => {
@@ -48,15 +29,11 @@ const App: () => React$Node = () => {
     .configure()
     .localNotificationSchedule({
 
-        // title: titulos[(Math.floor(Math.random() * 10 + 1))],
-        // message: textos[(Math.floor(Math.random() * 10 + 1))],
-        title: 'are you having nightmares?',
-        message: 'this is happening to several people, it is usually due to the stress of this situation, so try to calm down',
+        title: titulos[getRandom(2)],
+        message: textos[getRandom(2)],
         color: 'red',
         vibration: 450,
-        // repeatType: "time",
-        // repeatTime: 4000,
-        date: new Date(Date.now() + 1 )
+        date: new Date(Date.now() + 1 )//lembrar que da pra agendar por aqui
 
     });
 
@@ -85,24 +62,48 @@ const App: () => React$Node = () => {
 
             </View>
 
-            
-
         </View>
 
-        <View style = {styles.viewStyle}>
+        <ScrollView>
+            
+                <View style = {styles.viewStyle}>
 
-            <Text style = {styles.h2}> After clicking the button below, you will receive notifications with cool messages during your day </Text>
+                    <Text style = {styles.h2}> After clicking the button below, you will receive notifications with cool messages during your day </Text>
 
 
-            <View style= {styles.viewButton} onPress={createNotification} >
+                    <View style= {styles.viewButton} onPress={createNotification} >
 
-                <Text style = {styles.button} onPress={createNotification} >Click me</Text>
+                        <Text style = {styles.button} onPress={createNotification} >Click me</Text>
+
+                    </View>
+
 
             </View>
 
-            {/* <Text style = {styles.h2} >You can also write messages to other people</Text> */}
 
-        </View>
+            <View style = {styles.viewT2}>
+
+                <Text style = {styles.h2}> You can also write a message that can reach a person anywhere in the world </Text>
+
+                <TextInput
+                
+                    placeholder =  '            write a message'
+                    style = {styles.textInput}
+
+                />
+
+                <View style= {styles.viewButtonSend} onPress={createNotification} >
+
+                    <Text style = {styles.button} onPress={createNotification} >Submit</Text>
+
+                </View>
+
+                    <Text style = {styles.h5P2} onPress={createNotification} >It will be evaluated before reaching another person. Remember to be nice and kind</Text>
+
+            </View>
+
+
+        </ScrollView>
 
     </>
 
@@ -147,6 +148,15 @@ const styles = StyleSheet.create({
         height: 30
 
     },
+    viewT2: {
+
+        marginTop: 200,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10
+
+    },
     h1: {
 
         fontSize: 50,
@@ -165,12 +175,42 @@ const styles = StyleSheet.create({
         color: "white",
 
     },
+    h5P2: {
+
+        fontSize: 10,
+        color: "black",
+        marginTop: 15
+
+    },
     button: {
 
         fontSize: 30,
-        color: "white"
+        color: "white",
 
     },
+
+    viewButtonSend: {
+
+        marginTop: 10,
+        backgroundColor: "rgb( 117,93,196 )",
+        width: 150,
+        height: 50,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 10,
+        flex: 1 ,
+        alignSelf: 'flex-end'
+
+    },
+
     header: {
 
         backgroundColor: "rgb( 117,93,196 )",
@@ -189,6 +229,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
 
+    textInput: {
+
+        height: 80,
+        padding: 30,
+        width: 500,
+        backgroundColor: '#00000011',
+        borderRadius: 20
+
+    }
 });
 
 export default App;
